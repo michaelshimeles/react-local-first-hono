80243fba57f9a0e5d53d38d1063a9e2d660b9d73
Michael Shimeles <michaelwasihun96@gmail.com>
2024-12-02 00:28:08 -0500

    index

--- server/index.ts
+++ server/index.ts
@@ -307,8 +307,9 @@ const userUpdate = async ({
 };
 
 // Serve static assets first
-app.use("*", serveStatic({ root: "../frontend/dist" }));
+// app.use("*", serveStatic({ root: "../frontend/dist" }));
 app.use("*", serveStatic({ root: "../frontend/dist/index.html" }));
+
 
 export type AppType = typeof apiRoutes;
 
